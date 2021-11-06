cfc4159053248458a5843f024c5746d67cb5a70b
ariox41 <ariox41@gmail.com>
2021-11-06 11:14:13 +0300

    Conversion to Functional objects

--- src/include/tmdesc/tuple/tuple.hpp
+++ src/include/tmdesc/tuple/tuple.hpp
@@ -21,6 +21,11 @@ template <class... Ts> struct tuple : public detail::tuple_impl<std::make_index_
     constexpr tuple& operator=(const tuple&) = default;
     constexpr tuple& operator=(tuple&&) = default;
 
+    /// \code
+    /// template<class... Args> tuple(Args&&...);
+    /// template<class... Args> tuple(const tuple<Args...>&);
+    /// template<class... Args> tuple(tuple<Args...>&&);
+    /// \endcode
     using detail::tuple_impl<std::make_index_sequence<sizeof...(Ts)>, Ts...>::tuple_impl;
     using detail::tuple_impl<std::make_index_sequence<sizeof...(Ts)>, Ts...>::operator=;
 };
@@ -32,17 +37,26 @@ template <class T> constexpr bool is_tuple_v = is_tuple<T>::value;
 template <class... Ts>
 struct tuple_size<tuple<Ts...>, std::enable_if_t<true>> : public std::integral_constant<std::size_t, sizeof...(Ts)> {};
 
-template <class... Ts>
-constexpr tuple<unwrap_ref_decay_t<Ts>...>
-make_tuple(Ts&&... ts) noexcept(meta::conjunction_v<std::is_nothrow_constructible<unwrap_ref_decay_t<Ts>, Ts>...>) {
-    return tuple<unwrap_ref_decay_t<Ts>...>{std::forward<Ts>(ts)...};
-}
+struct make_tuple_t {
+    template <class... Ts>
+    constexpr tuple<unwrap_ref_decay_t<Ts>...> operator()(Ts&&... ts) const
+        noexcept(meta::conjunction_v<std::is_nothrow_constructible<unwrap_ref_decay_t<Ts>, Ts>...>) {
+        return tuple<unwrap_ref_decay_t<Ts>...>{std::forward<Ts>(ts)...};
+    }
+};
+constexpr make_tuple_t make_tuple{};
 
-template <class... Ts> constexpr tuple<Ts&...> tie(Ts&... ts) noexcept { return {ts...}; }
+struct tie_t {
+    template <class... Ts> constexpr tuple<Ts&...> operator()(Ts&... ts) const noexcept { return {ts...}; }
+};
+constexpr tie_t tie{};
 
-template <class... Ts> constexpr tuple<Ts&&...> forward_as_tuple(Ts&&... ts) noexcept {
-    return {std::forward<Ts>(ts)...};
-}
+struct forward_as_tuple_t {
+    template <class... Ts> constexpr tuple<Ts&&...> operator()(Ts&&... ts) const noexcept {
+        return {std::forward<Ts>(ts)...};
+    }
+};
+constexpr forward_as_tuple_t forward_as_tuple{};
 
 template <class... Ts> struct tuple_getter<tuple<Ts...>> { using type = detail::getter_by_id_for_tuple_t; };
 template <class... Ts> struct tuple_getter_by_type<tuple<Ts...>> { using type = detail::getter_by_type_for_tuple_t; };
