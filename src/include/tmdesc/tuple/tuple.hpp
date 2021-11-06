5c36caeb100a27368ae1ec374cef0d80ab5035ab
ariox41 <ariox41@gmail.com>
2021-11-06 11:03:24 +0300

    append forward_as_tuple

--- src/include/tmdesc/tuple/tuple.hpp
+++ src/include/tmdesc/tuple/tuple.hpp
@@ -14,8 +14,7 @@
 
 namespace tmdesc {
 /// full constexpr inherit-based tuple without recursive implementation
-template <class... Ts>
-struct tuple : public detail::tuple_impl<std::make_index_sequence<sizeof...(Ts)>, Ts...> {
+template <class... Ts> struct tuple : public detail::tuple_impl<std::make_index_sequence<sizeof...(Ts)>, Ts...> {
     constexpr tuple()             = default;
     constexpr tuple(const tuple&) = default;
     constexpr tuple(tuple&&)      = default;
@@ -31,21 +30,20 @@ template <class... Ts> struct is_tuple<tuple<Ts...>> : std::true_type {};
 template <class T> constexpr bool is_tuple_v = is_tuple<T>::value;
 
 template <class... Ts>
-struct tuple_size<tuple<Ts...>, std::enable_if_t<true>>
-  : public std::integral_constant<std::size_t, sizeof...(Ts)> {};
+struct tuple_size<tuple<Ts...>, std::enable_if_t<true>> : public std::integral_constant<std::size_t, sizeof...(Ts)> {};
 
 template <class... Ts>
-constexpr tuple<unwrap_ref_decay_t<Ts>...> make_tuple(Ts&&... ts) noexcept(
-    meta::conjunction_v<std::is_nothrow_constructible<unwrap_ref_decay_t<Ts>, Ts>...>) {
+constexpr tuple<unwrap_ref_decay_t<Ts>...>
+make_tuple(Ts&&... ts) noexcept(meta::conjunction_v<std::is_nothrow_constructible<unwrap_ref_decay_t<Ts>, Ts>...>) {
     return tuple<unwrap_ref_decay_t<Ts>...>{std::forward<Ts>(ts)...};
 }
 
 template <class... Ts> constexpr tuple<Ts&...> tie(Ts&... ts) noexcept { return {ts...}; }
 
-template <class... Ts> struct tuple_getter<tuple<Ts...>> {
-    using type = detail::getter_by_id_for_tuple_t;
-};
-template <class... Ts> struct tuple_getter_by_type<tuple<Ts...>> {
-    using type = detail::getter_by_type_for_tuple_t;
-};
+template <class... Ts> constexpr tuple<Ts&&...> forward_as_tuple(Ts&&... ts) noexcept {
+    return {std::forward<Ts>(ts)...};
+}
+
+template <class... Ts> struct tuple_getter<tuple<Ts...>> { using type = detail::getter_by_id_for_tuple_t; };
+template <class... Ts> struct tuple_getter_by_type<tuple<Ts...>> { using type = detail::getter_by_type_for_tuple_t; };
 } // namespace tmdesc
