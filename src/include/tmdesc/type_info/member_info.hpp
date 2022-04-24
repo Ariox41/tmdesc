35e72bec5d069a76c07e55914d55c35632206ffc
ariox41 <ariox41@gmail.com>
2022-04-24 10:39:04 +0300

    partial implementation on boost hana. Not working yet.

---
+++ src/include/tmdesc/type_info/member_info.hpp
@@ -0,0 +1,37 @@
+// Copyright Victor Smirnov 2021-2022
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+//
+// The documentation can be found at the library's page:
+// https://github.com/Ariox41/tmdesc
+#pragma once
+#include "../string_view.hpp"
+#include <boost/hana/map.hpp>
+namespace tmdesc {
+
+/// Contains the name of the member,
+/// a functional object for getting a reference to the member from the owner object,
+/// and a set of attributes of the member.
+template <class T, class Getter, class AS> struct member_info {
+    using value_type = T;
+
+    constexpr member_info(zstring_view name, Getter getter, AS attributes)
+      : member_name_(name)
+      , getter_(std::move(getter))
+      , attributes_(std::move(attributes)) {}
+
+    /// \return member name.
+    constexpr zstring_view name() const noexcept { return member_name_; }
+
+    /// \return functional object for getting a reference to the member from the owner object.
+    const Getter& getter() const noexcept { return getter_; }
+
+    /// \return `map<pair<type<Tag>, Value>...>` of member attributes
+    constexpr const AS& attributes() const noexcept { return attributes_; }
+
+private:
+    zstring_view member_name_;
+    Getter getter_;
+    AS attributes_;
+};
+} // namespace tmdesc
