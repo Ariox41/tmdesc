19be6e51c7cc41c3914664905efcdc57732366cc
ariox41 <ariox41@gmail.com>
2022-05-01 19:11:41 +0300

    remove custom tuple lib implementation

--- src/include/tmdesc/algorithm/unpack.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright Victor Smirnov 2021
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-//
-// The documentation can be found at the library's page:
-// https://github.com/Ariox41/tmdesc
-
-#pragma once
-#include "../concepts/foldable.hpp"
