b72552e2331c66eeaed0cf708a9bec414d9d3643
ariox41 <ariox41@gmail.com>
2022-05-01 19:11:20 +0300

    remove custom tuple lib implementation

--- src/include/tmdesc/type_info/type_info.hpp
+++ src/include/tmdesc/type_info/type_info.hpp
@@ -6,21 +6,20 @@
 // https://github.com/Ariox41/tmdesc
 #include "../string_view.hpp"
 #include <boost/hana/optional.hpp>
-namespace tmdesc {
+namespace tmdesc { 
 
-/** Contains information about type T.
-    A type can provide information about the set of its members and information about specific attributes of the type.
-
-    Type attributes are represented as a map {tag->value}, where tag is the type (identifier)
-    of the attribute, value is an arbitrary value specified for this attribute.
-    The set of attributes may be empty.
-
-    The set of members is represented as `nothing` or `just(tuple<member_info<unspecified>>{})`.
-    There is a difference between an empty list of members, and no list.
-    For example, a structure with an empty list of members corresponds to the xml representation `<Struct Name />`.
-    If the list of members is not specified, this structure cannot be serialized in xml.
-    But another sterilization method can be specified in the type attributes.
-*/
+/// Contains information about type T.
+///
+/// @details
+/// A type can provide information about the set of its members and information about specific attributes of the type.
+/// Type attributes are represented as a map {tag->value}, where tag is the type (identifier)
+/// of the attribute, value is an arbitrary value specified for this attribute.
+/// The set of attributes may be empty.
+/// The set of members is represented as `nothing` or `just(tuple<member_info<unspecified>>{})`.
+/// There is a difference between an empty list of members, and no list.
+/// For example, a structure with an empty list of members corresponds to the xml representation `<Struct Name />`.
+/// If the list of members is not specified, this structure cannot be serialized in xml.
+/// But another sterilization method can be specified in the type attributes.
 template <class T, class MS, class AS> struct type_info {
     constexpr type_info(MS members, AS attributes)
       : members_(std::move(members))
